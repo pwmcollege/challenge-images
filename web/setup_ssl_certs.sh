4f12efc0dcfdb01520dc46ee9a014ac9ee496a86
Fluffy <52132640+fluffy-polaris@users.noreply.github.com>
2026-07-29 00:37:02 -0700

    Update setup_ssl_certs.sh

--- web/setup_ssl_certs.sh
+++ web/setup_ssl_certs.sh
@@ -15,7 +15,7 @@ openssl req -x509 -new -nodes -key /opt/ca/internalCA.key -sha256 -days 3650 \
 openssl genrsa -out /opt/ca/internal.key.pem 2048
 openssl req -new -key /opt/ca/internal.key.pem -subj "/CN=cdn.internal" -out /opt/ca/internal.csr.pem
 
-printf "subjectAltName=DNS:*.internal\nkeyUsage=digitalSignature,keyEncipherment\nextendedKeyUsage=serverAuth" > /opt/ca/san.cnf
+printf "subjectAltName=DNS:proxy.internal,DNS:cdn.internal,DNS:challenge.internal,DNS:victim.internal\nkeyUsage=digitalSignature,keyEncipherment\nextendedKeyUsage=serverAuth" > /opt/ca/san.cnf
 openssl x509 -req -in /opt/ca/internal.csr.pem \
     -CA /opt/ca/internalCA.crt -CAkey /opt/ca/internalCA.key -CAcreateserial \
     -out /etc/ssl/certs/internal.cert.pem -days 825 -sha256 \
